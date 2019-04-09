4047b1b4d5ac2a2233798294ee686445e239fd40
Errol <erroldee2002@gmail.com>
2019-04-10 02:36:03 +0800

    portfolio work

---
+++ portfolio/src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+import Projects from "./Projects";
+
+class App extends Component {
+    state = {
+        displayBio: false
+    };
+
+    toggleDisplayBio = () => {
+        this.setState({
+            displayBio: !this.state.displayBio
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Errol. I'm a software engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in the Philippines, and code every day.</p>
+                            <p>My favorite language is JavaScript, and I think React.js is awesome.</p>
+                            <p>Besides coding, I also love gaming and eating!</p>
+                            <button onClick={this.toggleDisplayBio}>Show less</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )
+                }
+                <hr />
+                <Projects />
+            </div>
+        );
+    }
+}
+
+export default App;
